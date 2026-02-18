4b401d0bb724682bff58eb54b9c3b6b6f0596282
nick <8650180292@my.lerna.by>
2026-02-18 14:24:16 +0300

    update schemas import

---
+++ apps/clients/schemas/__init__.py
@@ -0,0 +1,13 @@
+from .client import ClientCreate, ClientOut, ClientUpdate
+from .contacts import ClientContactInfo, ClientContactInfoUpdate, ContactPersonSchema
+from .filters import ClientFilter
+
+__all__ = [
+    "ClientCreate",
+    "ClientOut",
+    "ClientUpdate",
+    "ClientContactInfo",
+    "ClientContactInfoUpdate",
+    "ContactPersonSchema",
+    "ClientFilter",
+]
