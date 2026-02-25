2a2573478ee1b91cd7daf682f1698b7226f99c73
nick <8650180292@my.lerna.by>
2026-02-25 11:34:47 +0300

    add pghistory context

--- config/celery.py
+++ config/celery.py
@@ -1,15 +1,34 @@
 import os
 
-from celery import Celery
+from celery import Celery, Task
+
+# Используем pghistory для трекинга контекста celery tasks
+from pghistory import context as pghistory_context
 
 # Устанавливаем переменную окружения, чтобы Celery знал, где искать настройки Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+
+class PghistoryTask(Task):
+    """
+    Базовый класс задач Celery с интеграцией pghistory.
+    Автоматически добавляет имя задачи в контекст аудита.
+    """
+
+    def __call__(self, *args, **kwargs):
+        # В БД в поле pgh_context будет: {"app_source": "Celery", "celery_task": "..."}
+        with pghistory_context(app_source="Celery", celery_task=self.name):
+            return super().__call__(*args, **kwargs)
+
 
 # Создаем экземпляр приложения Celery
 app = Celery("kronon")
 
 # Загружаем конфигурацию из настроек Django
 app.config_from_object("django.conf:settings", namespace="CELERY")
+
+# Подменяем базовый класс задачи
+app.Task = PghistoryTask
 
 # Автоматически обнаруживаем и регистрируем задачи из всех файлов tasks.py в установленных приложениях Django
 app.autodiscover_tasks()
--- manage.py
+++ manage.py
@@ -3,6 +3,10 @@
 
 import os
 import sys
+from contextlib import ExitStack
+
+# Используем pghistory для трекинга контекста консольных команд
+from pghistory import context as pghistory_context
 
 
 def main() -> None:
@@ -16,7 +20,23 @@ def main() -> None:
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
+
+    # --- PGHISTORY CONTEXT INSTRUMENTATION ---
+    # Игнорируем runserver (слишком много шума), миграции (конфликты схем) и тесты
+    if (
+        len(sys.argv) > 1
+        and not sys.argv[1].startswith("runserver")
+        and sys.argv[1] not in ["migrate", "makemigrations", "test"]
+    ):
+        # Оборачиваем выполнение команды в контекст
+        # В БД в поле pgh_context будет: {"app_source": "CLI", "command": "..."}
+        history_context = pghistory_context(app_source="CLI", command=" ".join(sys.argv[1:]))
+    else:
+        # Пустой контекстный менеджер, если трекинг не нужен
+        history_context = ExitStack()
+
+    with history_context:
+        execute_from_command_line(sys.argv)
 
 
 if __name__ == "__main__":
