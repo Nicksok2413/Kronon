915ce83a5e2fbc1067e806fcfa61a4620a65510b
nick <nicksok2413@gmail.com>
2026-03-02 15:20:10 +0000

    update AsyncApiKeyAuth

--- apps/common/auth.py
+++ apps/common/auth.py
@@ -10,7 +10,7 @@ from ninja.security import APIKeyHeader
 class AsyncApiKeyAuth(APIKeyHeader):
     """
     Асинхронная аутентификация по API-ключу через заголовок 'X-API-Key'.
-    Используется для межсервисного взаимодействия или вебхуков.
+    Используется для межсервисного взаимодействия.
     """
 
     param_name = "X-API-Key"
@@ -18,16 +18,16 @@ class AsyncApiKeyAuth(APIKeyHeader):
     async def authenticate(self, request: HttpRequest, key: str) -> str | None:
         """
         Проверяет переданный ключ.
-        Возвращает строку (помечает пользователя), если ключ верен, иначе None.
+        Если ключ верен, возвращает строку (маркер для системы), что запрос пришел от системы, иначе None.
 
         Args:
             request (HttpRequest): Объект входящего запроса.
             key (str): Переданный API-ключ.
 
         Returns:
-            str | None: Метка пользователя или None.
+            str | None: Маркер для системы
         """
         if settings.INTERNAL_API_KEY and key == settings.INTERNAL_API_KEY:
-            return "api_key_user"  # Условный маркер, что запрос пришел от системы
+            return "system_api"  # Маркер для системы, что это программный доступ
 
         return None
