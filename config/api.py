b95a566672f38ebe837652fc574cc141dfa8ab3d
nick <8650180292@my.lerna.by>
2026-03-18 16:54:10 +0300

    modifying pghistory

--- config/api.py
+++ config/api.py
@@ -2,13 +2,52 @@
 Root API Configuration.
 """
 
+# from typing import Callable
+
+# from django.http import HttpRequest, HttpResponse
 from ninja_extra import NinjaExtraAPI
 from ninja_jwt.authentication import AsyncJWTAuth
 from ninja_jwt.controller import NinjaJWTDefaultController
 
+# from pghistory import context as pghistory_context
 from apps.audit.api.v1 import router as audit_router
 from apps.clients.api.v1 import router as clients_router
+from apps.common.auth import AsyncApiKeyAuth
 from apps.common.exceptions import setup_exception_handlers
+
+# from apps.users.constants import SYSTEM_USER_ID
+
+
+# def ninja_audit_user_patch(request: HttpRequest, get_response: Callable) -> HttpResponse:
+#     """
+#     Патч-middleware для NinjaExtraAPI для синхронизации контекста аудита.
+#
+#     Захватывает данные аутентификации (JWT/API-Key) и дополняет контекст пользователем (user).
+#
+#     Args:
+#         request: Объект входящего HTTP запроса.
+#         get_response: Функция для продолжения цепочки обработки.
+#
+#     Returns:
+#         Объект ответа с установленным пользователем (user).
+#     """
+#
+#     # Выполняем запрос (здесь Ninja проверит Auth и заполнит request.user)
+#     response = get_response(request)
+#
+#     user = request.user.id if request.user.is_authenticated else None
+#
+#     # Определяем инициатора запроса (User или System API)
+#     if getattr(request, "auth", None) == "system_api":
+#         user = SYSTEM_USER_ID
+#
+#     # Дополняем контекст pghistory (объединяется с контекстом из Django Middleware)
+#     # pghistory.context умеет обновлять существующий контекст в рамках одного потока
+#     with pghistory_context(user=user):
+#         return response
+#
+# api.add_middleware(ninja_audit_user_patch)
+
 
 # Инициализируем API
 # NinjaExtraAPI дает больше возможностей, чем просто NinjaAPI
@@ -17,7 +56,7 @@ api = NinjaExtraAPI(
     version="1.0.0",
     description="Enterprise Accounting OS API",
     urls_namespace="api",  # Важно для reverse()
-    auth=AsyncJWTAuth(),  # JWT по умолчанию для всех эндпоинтов (кроме тех, где auth=None)
+    auth=[AsyncJWTAuth(), AsyncApiKeyAuth()],  # Аутентификация по умолчанию для всех эндпоинтов
 )
 
 
